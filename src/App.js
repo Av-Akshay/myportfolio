fc079f2f1459fb0ca2878f5527bced78ddcb30c8
Av-Akshay <akshuchauhan1jan@gmail.com>
2023-09-12 00:41:13 -0700

    setup is done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <section className="center-div"></section>
+    </div>
+  );
+}
+
+export default App;
