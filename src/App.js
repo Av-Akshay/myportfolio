80850d5f23ac6916a294f5a9648d4040ee6c20ab
Av-Akshay <akshuchauhan1jan@gmail.com>
2023-09-13 09:27:53 -0700

    All pages is done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import About from "./pages/AboutPage";
 import Project from "./pages/ProjesctsPage";
